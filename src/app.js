8636a531ae96cc905943f35df82cb7f15495e193
jamesgolin <jamesagolin@gmail.com>
2016-12-09 07:44:55 -0500

    add react

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+class App extends React.Component {
+    render() {
+        return <p>Hello From My Component</p>;
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from 'app.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <App />,
+      document.getElementById("root")
+    );
+})

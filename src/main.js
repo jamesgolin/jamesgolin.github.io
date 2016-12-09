0c3936e1ae682b8fe8d1dfed47fbfdd462c2c3be
jamesgolin <jamesagolin@gmail.com>
2016-12-09 07:49:16 -0500

    update main

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './app.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(
-      <App />,
-      document.getElementById("root")
-    );
-})
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);

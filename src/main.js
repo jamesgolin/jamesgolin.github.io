5fcf83ff8ff87200e109b228345095e933fd83f5
James Golin <james@komand.com>
2016-12-09 13:20:16 -0500

    fix bug

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './app.jsx'
 
 document.addEventListener('DOMComponentLoaded', () => {
-    React.render(
+    ReactDOM.render(
         <App />,
         document.getElementById("root")
     );

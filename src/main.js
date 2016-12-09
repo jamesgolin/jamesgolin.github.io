72c726f263d845048d2926a52807e9903a6c1ef1
jamesgolin <jamesagolin@gmail.com>
2016-12-09 09:13:26 -0500

    fix main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './app.jsx'
 
 document.addEventListener('DOMContentLoaded', () => {
-    React.render(
+    ReactDOM.render(
         <App />,
         document.getElementByID("root")
     );
-})
+});
